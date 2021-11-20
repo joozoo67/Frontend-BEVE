89f5fdc2b859a407b3625ec2701c40c274c51ff0
spig0126 <spig0126@gmail.com>
2021-11-20 18:37:46 +0900

    Create FitlerTypeBox.js child component for Filter.js

--- src/components/filter/FilterTypeBox.js
+++ src/components/filter/FilterTypeBox.js
@@ -1,18 +1,24 @@
-import { Text, HStack, Box, Button } from "@chakra-ui/react";
+import styled from "styled-components";
+import { Text, HStack, Button } from "@chakra-ui/react";
 
-export default function FilterTypeBox({type, options}) {
-    const stack = options.map((option, index) => {
-        <Button variant="outline" key="index">
-            {option}
-        </Button>
-    });
+export default function FilterTypeBox({ type, options }) {
+  console.log(options);
+  const stack = options.map((option, index) => (
+    <Button variant="outline" key="index">
+      {option}
+    </Button>
+  ));
 
-    return(
-        <Box>
-            <Text>{type}</Text>
-            <HStack spacing="15px" shouldWrapChildren="true">
-                {stack}
-            </HStack>
-        </Box>
-    );
+  return (
+    <TypeContainer>
+      <Text>{type}</Text>
+      <HStack spacing="15px" shouldWrapChildren="true" w="90%">
+        {stack}
+      </HStack>
+    </TypeContainer>
+  );
 }
+
+const TypeContainer = styled.div`
+  width: 70%;
+`;
