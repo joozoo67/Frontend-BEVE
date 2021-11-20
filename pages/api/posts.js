9a5cb64db4991ea079a4c4cc72f666c19163559e
newave986 <kingofthefall538@gmail.com>
2021-11-21 01:42:42 +0900

    add : add post req

---
+++ pages/api/posts.js
@@ -0,0 +1,18 @@
+import { ObjectId } from "mongodb";
+import clientPromise from "../../lib/mongodb";
+
+export default async function handler(req, res) {
+
+  const query = req.query.res_id;
+  // const query = 1;
+
+  const client = await clientPromise;
+  const db = client.db("sample");
+
+  const data = await db
+    .collection("sam")
+    .findOne({ _id: new ObjectId(query) });
+
+  res.json(data);
+  return <div>data</div>;
+}
