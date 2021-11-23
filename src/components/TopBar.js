0539c5241216fba65392b301eac4c0d0751d3138
spig0126 <spig0126@gmail.com>
2021-11-24 05:20:39 +0900

    ✨ Move to index page when logo is clicked

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,9 +1,20 @@
-import { Flex, Image } from "@chakra-ui/react";
-import logo from "../../public/logo.svg"
+import { Flex, Button, Image } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import logo from "../../public/logo.svg";
 export default function TopBar() {
+  const router = useRouter();
+
   return (
-    <Flex color="white" w="100%">
-      <Image boxSize="150px" src={logo}/>
+    <Flex color="white" w="100%" h="95px">
+      <Button
+        size="150px"
+        variant="ghost"
+        _hover="none"
+        _focus="none"
+        onClick={() => router.push("/")}
+      >
+        <Image boxSize="150px" src={logo} />
+      </Button>
     </Flex>
   );
 }
