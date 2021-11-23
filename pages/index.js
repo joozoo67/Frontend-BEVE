1485cc7309e0651de5f11793b58dc709c496a833
spig0126 <spig0126@gmail.com>
2021-11-24 05:27:21 +0900

    💄 Change css and update parameters

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,32 @@
 import TopBar from "../src/components/TopBar";
-import MainSearchForm from "../src/components/MainSearchForm";
-import Filter from "../src/components/filter/Filter";
+import SearchForm from "../src/components/SearchForm";
 import { Flex, Center } from "@chakra-ui/react";
-import { useState } from "react";
-
+import NearShopsBox from "../src/components/NearShopsBox";
 
 export default function Home() {
   return (
     <>
       <TopBar />
-      <Flex flexDirection="column" bgColor="green.200" w="100%" h="45vh" minH="400px" pos="absolute" top="100" pt="20%">
-        <Center fontSize="5xl">Explore Seoul with Vegetarian Restaurants</Center>
+      <Flex
+        flexDirection="column"
+        bgColor="green.200"
+        w="100%"
+        h="45%"
+        minH="500px"
+        p="15% 0 3% 0"
+      >
+        <Center fontSize="5xl">
+          Explore Seoul with Vegetarian Restaurants
+        </Center>
         <Center fontSize="3xl">BECOME VEGETARIAN</Center>
-        <MainSearchForm />
+        <SearchForm
+          width="60%"
+          marginTop="40px"
+          inputVariant="filled"
+          iconButtonVariant="solid"
+        />
       </Flex>
-      
+      <NearShopsBox />
     </>
   );
 }
