929ae2d596749bd899d91d63fceb8c9b421adba9
spig0126 <spig0126@gmail.com>
2021-11-24 01:44:08 +0900

    💄 Add Box with background and positioning

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,18 @@
+import TopBar from "../src/components/TopBar";
+import MainSearchForm from "../src/components/MainSearchForm";
+import Filter from "../src/components/filter/Filter";
+import { Box } from "@chakra-ui/react";
+import { useState } from "react";
+
+
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <>
+      <TopBar />
+      <Box bgColor="green.200" w="100%" h="50vh" pos="absolute" top="100">
+        <MainSearchForm />
+      </Box>
+      
+    </>
+  );
 }
