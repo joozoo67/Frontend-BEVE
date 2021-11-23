b66c5bcb696ef01db8c084635f7e6438642f3824
spig0126 <spig0126@gmail.com>
2021-11-24 02:22:44 +0900

    💄 Change Box to Flex, Text to Center, and other css

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import TopBar from "../src/components/TopBar";
 import MainSearchForm from "../src/components/MainSearchForm";
 import Filter from "../src/components/filter/Filter";
-import { Box } from "@chakra-ui/react";
+import { Flex, Center } from "@chakra-ui/react";
 import { useState } from "react";
 
 
@@ -9,9 +9,11 @@ export default function Home() {
   return (
     <>
       <TopBar />
-      <Box bgColor="green.200" w="100%" h="50vh" pos="absolute" top="100">
+      <Flex flexDirection="column" bgColor="green.200" w="100%" h="45vh" minH="400px" pos="absolute" top="100" pt="20%">
+        <Center fontSize="5xl">Explore Seoul with Vegetarian Restaurants</Center>
+        <Center fontsize="3xl">BECOME VEGETARIAN</Center>
         <MainSearchForm />
-      </Box>
+      </Flex>
       
     </>
   );
