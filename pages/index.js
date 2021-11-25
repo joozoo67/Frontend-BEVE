d0fb67124ad3fe06a869c24aa4a7654e57f7cdc8
spig0126 <spig0126@gmail.com>
2021-11-25 23:48:33 +0900

    ⏪ Bring back code before merge

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,32 @@
-import CardGrid from "../src/components/CardGrid";
+import TopBar from "../src/components/TopBar";
+import SearchForm from "../src/components/SearchForm";
+import { Flex, Center } from "@chakra-ui/react";
+import NearShopsBox from "../src/components/NearShopsBox";
 
 export default function Home() {
   return (
-    <div>
-      <CardGrid />
-    </div>
+    <>
+      <TopBar />
+      <Flex
+        flexDirection="column"
+        bgColor="green.200"
+        w="100%"
+        h="45%"
+        minH="500px"
+        p="15% 0 3% 0"
+      >
+        <Center fontSize="5xl">
+          Explore Seoul with Vegetarian Restaurants
+        </Center>
+        <Center fontSize="3xl">BECOME VEGETARIAN</Center>
+        <SearchForm
+          width="60%"
+          marginTop="40px"
+          inputVariant="filled"
+          iconButtonVariant="solid"
+        />
+      </Flex>
+      <NearShopsBox />
+    </>
   );
 }
