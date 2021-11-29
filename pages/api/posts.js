2453edc9c78422962834f529e75454f953992248
newave986 <kingofthefall538@gmail.com>
2021-11-30 02:29:10 +0900

    fix : get query and search

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -3,16 +3,38 @@ import clientPromise from "../../lib/mongodb";
 
 export default async function handler(req, res) {
 
-  const query = req.query.stage;
-  console.log(query)
+  const area_query = req.query.area.replace(/[{|}|']+/g, '');
+  const stage_query = req.query.stage.replace(/[{|}|']+/g, '');
+  const type_query = req.query.type.replace(/[{|}|']+/g, '');
+  const inputText_query = req.query.inputText;
+  const query_s = {area_query, stage_query, type_query, inputText_query};
 
-  const client = await clientPromise;
-  const db = client.db("sample");
+  var dict = {};
 
-  const data = await db
-    .collection("sam")
-    .findOne({ _id: new ObjectId(query) });
+  if (area_query != {}) dict["location.region"] = area_query;
+  if (type_query != {}) dict["category"] = type_query;
 
-  res.json(data);
+  console.log(dict);
+
+    const client = await clientPromise;
+    const db = client.db("sample");
+
+    const data = await db
+       .collection("sam")
+       .find(dict)
+       // .find( {'location.region':  "강남구"} )
+       // .find( {category: "한식", location: {region: "강남구"}} )
+       .toArray();
+
+      res.json(data);
+
+//  const db = client.db("sample");
+//
+//  const data = await db
+//    .collection("sam")
+//    .findOne({ _id: new ObjectId(query) });
+//
+//  res.json(data);
+
   return <div>data</div>;
 }
