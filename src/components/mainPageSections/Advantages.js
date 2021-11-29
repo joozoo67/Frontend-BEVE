11ffa089290b1765f446e0bf4b4b2f60d4664aa8
spig0126 <spig0126@gmail.com>
2021-11-29 14:07:21 +0900

    💄 Change styles

--- src/components/mainPageSections/Advantages.js
+++ src/components/mainPageSections/Advantages.js
@@ -2,7 +2,7 @@ import { Flex, Text } from "@chakra-ui/layout";
 
 export default function Advantages() {
   return (
-    <Flex w="100%" p="70px 15%" bgColor="#f4ecd5" color="black">
+    <Flex w="100%" p="70px 15%" mt="20px" bgColor="#f4ecd5" color="black">
       <Text fontSize="5xl" fontWeight="bold" w="30%">
         Why Be Vegetarian?
       </Text>
