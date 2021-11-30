a38f6057b8328896d0aa5a7614438021df6478a6
newave986 <kingofthefall538@gmail.com>
2021-11-30 12:24:07 +0900

    add : add data with db

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -7,34 +7,27 @@ export default async function handler(req, res) {
   const stage_query = req.query.stage.replace(/[{|}|']+/g, '');
   const type_query = req.query.type.replace(/[{|}|']+/g, '');
   const inputText_query = req.query.inputText;
-  const query_s = {area_query, stage_query, type_query, inputText_query};
+  // const query_s = {area_query, stage_query, type_query, inputText_query};
 
   var dict = {};
 
-  if (area_query != {}) dict["location.region"] = area_query;
-  if (type_query != {}) dict["category"] = type_query;
+  if (area_query !=  '') dict["location.region"] = area_query;
+  if (stage_query != '') dict["menu.level"] = stage_query;
+  if (type_query != '') dict["category"] = type_query;
 
   console.log(dict);
 
-    const client = await clientPromise;
-    const db = client.db("sample");
+  const client = await clientPromise;
+  const db = client.db("sample");
 
-    const data = await db
-       .collection("sam")
-       .find(dict)
-       // .find( {'location.region':  "강남구"} )
-       // .find( {category: "한식", location: {region: "강남구"}} )
-       .toArray();
+  const data = await db
+    .collection("sam")
+    .find(dict)
+    //.find( { "menu": {"$elemMatch": {"menu": "비건"}}})
+    // .find({"menu": {"level": "비건"}})
+    .toArray();
 
-      res.json(data);
-
-//  const db = client.db("sample");
-//
-//  const data = await db
-//    .collection("sam")
-//    .findOne({ _id: new ObjectId(query) });
-//
-//  res.json(data);
+  res.json(data);
 
   return <div>data</div>;
 }
