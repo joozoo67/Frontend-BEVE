9b46c7d7643d6f2be2fbfbf34f458455dd4b545e
spig0126 <spig0126@gmail.com>
2021-12-01 21:40:43 +0900

    🚧 Check if request is working

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -1,35 +1,33 @@
 import { ObjectId } from "mongodb";
 import clientPromise from "../../lib/mongodb";
 
-import { Box } from "@chakra-ui/react";
-
 export default async function handler(req, res) {
 
-  const area_query = req.query.area.replace(/[{|}|']+/g, '');
-  const stage_query = req.query.stage.replace(/[{|}|']+/g, '');
-  const type_query = req.query.type.replace(/[{|}|']+/g, '');
-  const inputText_query = req.query.inputText;
-  // const query_s = {area_query, stage_query, type_query, inputText_query};
-
-  var dict = {};
-
-  if (area_query !=  '') dict["location.region"] = area_query;
-  if (stage_query != '') dict["menu.level"] = stage_query;
-  if (type_query != '') dict["category"] = type_query;
-
-  console.log(dict);
+//  const area_query = req.query.area.replace(/[{|}|']+/g, '');
+//  const stage_query = req.query.stage.replace(/[{|}|']+/g, '');
+//  const type_query = req.query.type.replace(/[{|}|']+/g, '');
+//  const inputText_query = req.query.inputText;
+//  // const query_s = {area_query, stage_query, type_query, inputText_query};
+//
+//  var dict = {};
+//
+//  if (area_query !=  '') dict["location.region"] = area_query;
+//  if (stage_query != '') dict["menu.level"] = stage_query;
+//  if (type_query != '') dict["category"] = type_query;
+//
+//  console.log(dict);
 
   const client = await clientPromise;
   const db = client.db("sample");
 
   const data = await db
     .collection("sam")
-    .find(dict)
-    //.find( { "menu": {"$elemMatch": {"menu": "비건"}}})
-    // .find({"menu": {"level": "비건"}})
+    .find( { "location.region": "강동구" } )
     .toArray();
+
+  console.log(data);
 
   res.json(data);
 
-  return <Box>data</Box>;
+  return <div>data</div>;
 }
