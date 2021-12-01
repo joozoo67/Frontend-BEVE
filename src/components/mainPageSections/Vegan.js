d41c0247c09cac2b24a9b7aa705719d32d6c8370
joozoo67 <kylakim@naver.com>
2021-12-02 02:22:37 +0900

    Styles: change css

--- src/components/mainPageSections/Vegan.js
+++ src/components/mainPageSections/Vegan.js
@@ -7,7 +7,10 @@ export default function Vegan() {
         비건은 <br /> 채식주의의 <br /> 한 종류?
       </Text>
       <Text color="black" fontSize="2rem">
-        <br/>YES!<br/> 채식주의의 7단계 중 최상의 단계를 <br/>일컫는 말입니다. 
+        <br />
+        YES!
+        <br /> 채식주의의 7단계 중 최상의 단계를 <br />
+        일컫는 말입니다.
       </Text>
     </Box>
   );
--- src/components/mainPageSections/VegetarianStages.js
+++ src/components/mainPageSections/VegetarianStages.js
@@ -1,4 +1,4 @@
-import { Flex, Grid, GridItem, HStack, Text } from "@chakra-ui/layout";
+import { Flex, Grid, GridItem, HStack, Text, Wrap } from "@chakra-ui/layout";
 import {
   GiPlantSeed as VegetableIcon,
   GiMilkCarton as DairyIcon,
@@ -10,122 +10,145 @@ import { FaFish as FishIcon } from "react-icons/fa";
 
 export default function VegetarianStages() {
   return (
-    <Flex bgColor="#7ca83f" color="white" h="55rem" mt="20px">
-      <Grid
-        templateRows="repeat(8, 1fr)"
-        templateColumns="25rem 50rem"
-        gap={"3rem"}
-        justifyItems="flex-start"
-        w="50%"
-        h="50%"
-        pl="3rem"
-        pt="1rem"
-      >
-        <GridItem>
-        <Text color="white" fontSize="3rem" fontWeight="bold">
+    <Flex
+      bgColor="#7ca83f"
+      color="white"
+      mt="20px"
+      flexDir="column"
+      py="3%"
+      px={{ base: "3%", md: "7%" }}
+    >
+      <Text color="white" fontSize="3rem" fontWeight="bold">
         채식주의 단계?
       </Text>
-        </GridItem>
-
-        <GridItem>
-        <Text color="black" fontSize="1rem">
-        <br/>채식주의는 7가지 단계로 세분화되어 있습니다.<br/> 
-        그 기준은 유제품, 동물의 알, 물고기, 하얀 고기, 붉은 고기 등 동물성 음식의 종류에 따라 나뉩니다.
-        <br/>(색칠은 섭취 가능을 뜻합니다) 
+      <Text color="black" fontSize="1rem">
+        <br />
+        채식주의는 7가지 단계로 세분화되어 있습니다.
+        <br />
+        그 기준은 유제품, 동물의 알, 물고기, 하얀 고기, 붉은 고기 등 동물성
+        음식의 종류에 따라 나뉩니다.
+        <br />
+        (색칠은 섭취 가능을 뜻합니다)
       </Text>
-        </GridItem>
-
-        <GridItem>
+      <Grid
+        templateRows="repeat(8, 1fr)"
+        templateColumns="35% repeat(6, 1fr)"
+        justifyItems="flex-start"
+        w="100%"
+        h="50%"
+        px="0"
+        py="1%"
+      >
+        <GridItem colStart="0" colEnd="0" rowStart="1" rowEnd="1">
           비건
-          <br /> (Vegan)
+          <br />
+          (Vegan)
+        </GridItem>
+        <GridItem colStart="2" colEnd="2" rowStart="1" rowEnd="1">
+          <VegetableIcon size="1.5rem" color="2A7908" />
         </GridItem>
 
-        <GridItem>
-        <VegetableIcon size="1.5rem" color="2A7908"/>
-        </GridItem>
-
-        <GridItem>
+        <GridItem colStart="0" colEnd="0" rowStart="2" rowEnd="2">
           락토 베지테리언
-          <br /> (Lacto vegetarian)
+          <br />
+          (Lacto vegetarian)
+        </GridItem>
+        <GridItem colStart="2" colEnd="2" rowStart="2" rowEnd="2">
+          <VegetableIcon size="1.5rem" color="2A7908" />
+        </GridItem>
+        <GridItem colStart="3" colEnd="3" rowStart="2" rowEnd="2">
+          <DairyIcon size="1.5rem" color="#4BB6EC" />
         </GridItem>
 
-        <GridItem>
-        <HStack spacing='3rem'>
-        <VegetableIcon size="1.5rem" color="2A7908"/>
-        <DairyIcon size="1.5rem" color="#4BB6EC"/>
-        </HStack>
-        </GridItem>
-
-        <GridItem>
+        <GridItem colStart="1" colEnd="1" rowStart="3" rowEnd="3">
           오보 베지테리언
-          <br /> (Ovo vegetarian)
+          <br />
+          (Ovo vegetarian)
+        </GridItem>
+        <GridItem colStart="2" colEnd="2" rowStart="3" rowEnd="3">
+          <VegetableIcon size="1.5rem" color="2A7908" />
+        </GridItem>
+        <GridItem colStart="4" colEnd="4" rowStart="3" rowEnd="3">
+          <EggIcon size="1.5rem" color="yellow" />
         </GridItem>
 
-        <GridItem>
-          <HStack spacing='3rem'>
-        <VegetableIcon size="1.5rem" color="2A7908"/>
-        <EggIcon size="1.5rem" color="yellow"/>
-        </HStack>
-        </GridItem>
-
-        <GridItem>
+        <GridItem colStart="1" colEnd="1" rowStart="4" rowEnd="4">
           락토 오보 베지테리언
-          <br /> (Lacto-ovo vegetarian)
+          <br />
+          (Lacto-ovo vegetarian)
+        </GridItem>
+        <GridItem colStart="2" colEnd="2" rowStart="4" rowEnd="4">
+          <VegetableIcon size="1.5rem" color="2A7908" />
+        </GridItem>
+        <GridItem colStart="3" colEnd="3" rowStart="4" rowEnd="4">
+          <DairyIcon size="1.5rem" color="#4BB6EC" />
+        </GridItem>
+        <GridItem colStart="4" colEnd="4" rowStart="4" rowEnd="4">
+          <EggIcon size="1.5rem" color="yellow" />
         </GridItem>
 
-        <GridItem>
-        <HStack spacing='3rem'>
-        <VegetableIcon size="1.5rem" color="2A7908"/>
-        <DairyIcon size="1.5rem" color="#4BB6EC"/>
-        <EggIcon size="1.5rem" color="yellow"/>
-        </HStack>
-        </GridItem>
-
-        <GridItem>
+        <GridItem colStart="1" colEnd="1" rowStart="5" rowEnd="5">
           페스코 베지테리언
-          <br /> (Pesco-vegetarian)
+          <br />
+          (Pesco-vegetarian)
+        </GridItem>
+        <GridItem colStart="2" colEnd="2" rowStart="5" rowEnd="5">
+          <VegetableIcon size="1.5rem" color="2A7908" />
+        </GridItem>
+        <GridItem colStart="3" colEnd="3" rowStart="5" rowEnd="5">
+          <DairyIcon size="1.5rem" color="#4BB6EC" />
+        </GridItem>
+        <GridItem colStart="4" colEnd="4" rowStart="5" rowEnd="5">
+          <EggIcon size="1.5rem" color="yellow" />
+        </GridItem>
+        <GridItem colStart="5" colEnd="5" rowStart="5" rowEnd="5">
+          <FishIcon size="1.5rem" color="391DEE" />
         </GridItem>
 
-        <GridItem>
-        <HStack spacing='3rem'>
-        <VegetableIcon size="1.5rem" color="2A7908"/>
-        <DairyIcon size="1.5rem" color="#4BB6EC"/>
-        <EggIcon size="1.5rem" color="yellow"/>
-        <FishIcon size="1.5rem" color="391DEE"/>
-        </HStack>
-        </GridItem>
-
-        <GridItem>
+        <GridItem colStart="1" colEnd="1" rowStart="6" rowEnd="6">
           폴로 베지테리언
-          <br /> (Pollo-vegetarian)
+          <br />
+          (Pollo-vegetarian)
+        </GridItem>
+        <GridItem colStart="2" colEnd="2" rowStart="6" rowEnd="6">
+          <VegetableIcon size="1.5rem" color="2A7908" />
+        </GridItem>
+        <GridItem colStart="3" colEnd="3" rowStart="6" rowEnd="6">
+          <DairyIcon size="1.5rem" color="#4BB6EC" />
+        </GridItem>
+        <GridItem colStart="4" colEnd="4" rowStart="6" rowEnd="6">
+          <EggIcon size="1.5rem" color="yellow" />
+        </GridItem>
+        <GridItem colStart="5" colEnd="5" rowStart="6" rowEnd="6">
+          <FishIcon size="1.5rem" color="391DEE" />
+        </GridItem>
+        <GridItem colStart="6" colEnd="6" rowStart="6" rowEnd="6">
+          <PoultryIcon size="1.5rem" color="FF6A44" />
         </GridItem>
 
-        <GridItem>
-        <HStack spacing='3rem'>
-        <VegetableIcon size="1.5rem" color="2A7908"/>
-        <DairyIcon size="1.5rem" color="#4BB6EC"/>
-        <EggIcon size="1.5rem" color="yellow"/>
-        <FishIcon size="1.5rem" color="391DEE"/>
-        <PoultryIcon size="1.5rem" color="FF6A44"/>
-        </HStack>
+        <GridItem colStart="1" colEnd="1" rowStart="7" rowEnd="7">
+          폴로 베지테리언
+          <br />
+          (Pollo-vegetarian)
         </GridItem>
-
-        <GridItem>
-          플렉시테리언
-          <br /> (Flexitarion)
+        <GridItem colStart="2" colEnd="2" rowStart="7" rowEnd="7">
+          <VegetableIcon size="1.5rem" color="2A7908" />
         </GridItem>
-
-        <GridItem>
-        <HStack spacing='3rem'>
-        <VegetableIcon size="1.5rem" color="2A7908"/>
-        <DairyIcon size="1.5rem" color="#4BB6EC"/>
-        <EggIcon size="1.5rem" color="yellow"/>
-        <FishIcon size="1.5rem" color="391DEE"/>
-        <PoultryIcon size="1.5rem" color="FF6A44"/>
-        <MeatIcon size="1.5rem" color="E61818"/>
-        </HStack>
+        <GridItem colStart="3" colEnd="3" rowStart="7" rowEnd="7">
+          <DairyIcon size="1.5rem" color="#4BB6EC" />
         </GridItem>
-
+        <GridItem colStart="4" colEnd="4" rowStart="7" rowEnd="7">
+          <EggIcon size="1.5rem" color="yellow" />
+        </GridItem>
+        <GridItem colStart="5" colEnd="5" rowStart="7" rowEnd="7">
+          <FishIcon size="1.5rem" color="391DEE" />
+        </GridItem>
+        <GridItem colStart="6" colEnd="6" rowStart="7" rowEnd="7">
+          <PoultryIcon size="1.5rem" color="FF6A44" />
+        </GridItem>
+        <GridItem colStart="7" colEnd="7" rowStart="7" rowEnd="7">
+          <MeatIcon size="1.5rem" color="E61818" />
+        </GridItem>
       </Grid>
     </Flex>
   );
