47d7258bf9d4e9ec1d0de306cc73dc4ddf6a6e9b
spig0126 <spig0126@gmail.com>
2021-12-02 01:41:51 +0900

    📦 Apply react query to App

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,21 @@
 import * as React from "react";
-
 import { ChakraProvider } from "@chakra-ui/react";
+import { ReactQueryDevtools } from 'react-query/devtools'
+import {QueryClient, QueryClientProvider} from "react-query"
+
 import Fonts from "../theme/Fonts";
 import theme from "../theme/theme";
+
+const queryClient = new QueryClient();
 
 export default function App({ Component }) {
   return (
     <ChakraProvider theme={theme}>
-      <Fonts />
-      <Component />
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <Fonts />
+        <Component />
+      </QueryClientProvider>
     </ChakraProvider>
   );
 }
