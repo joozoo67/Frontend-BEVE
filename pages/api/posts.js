c8bd58b5be4e5246449b7b1b223b06b8ae0e8531
newave986 <kingofthefall538@gmail.com>
2021-12-03 03:21:23 +0900

    add : search full text #2

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -13,12 +13,14 @@ export default async function handler(req, res) {
   const type_query = req.query.type.replace(/[[|{|}|]|]| |'|"|']+/g, '');
   const type_query_list = type_query.split(",");
 
-  const inputText_query = req.query.inputText;
+  const inputText_query = req.query.inputText.replace(/[[|{|}|]|]| |'|"|']+/g, '');
+  const inputText_query_l = inputText_query.replace(",", ' ');
 
   var dict = {};
   var dict_area = {};
   var dict_type = {};
   var dict_stage = {};
+  var dict_input = {};
 
   if (area_query !=  '') {
     var tmp = []
@@ -35,16 +37,25 @@ export default async function handler(req, res) {
   if (stage_query != '') dict_stage["menu"] = { "$elemMatch": {"level": stage_query_list[stage_query_list.length-1]} };
   // {"menu": { "$elemMatch": {"level": stage_query}}} 형식으로 되어야 함
 
-  dict["$and"] = [ dict_area, dict_type, dict_stage ];
+  if (inputText_query != '') {
+      dict_input = { $text : { $search : inputText_query_l } };
+  }
+  // { $text : { $search : "java coffee" } 형식
+  console.log(dict_input);
+
+  dict["$and"] = [ dict_area, dict_type, dict_stage, dict_input ];
 
   if (area_query == '' && type_query == '' && stage_query == '' && inputText_query == '') dict["location.region"] = "오류";
 
   const client = await clientPromise;
   const db = client.db("sample");
 
+  db.collection("sam").createIndex( { "$**" : "text" } );
+
   const data = await db
     .collection("sam")
     .find(dict)
+    //.find({ $text : { $search : inputText_query_l } })
     .toArray();
 
   console.log(data);
