cad6a71f60fd471ae9faa4d195b303f01f5294f9
newave986 <kingofthefall538@gmail.com>
2021-12-02 23:46:30 +0900

    fix : fix to find vegan level of menu

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -3,26 +3,32 @@ import clientPromise from "../../lib/mongodb";
 
 export default async function handler(req, res) {
 
-//  const area_query = req.query.area.replace(/[{|}|']+/g, '');
-//  const stage_query = req.query.stage.replace(/[{|}|']+/g, '');
-//  const type_query = req.query.type.replace(/[{|}|']+/g, '');
-//  const inputText_query = req.query.inputText;
-//  // const query_s = {area_query, stage_query, type_query, inputText_query};
-//
-//  var dict = {};
-//
-//  if (area_query !=  '') dict["location.region"] = area_query;
-//  if (stage_query != '') dict["menu.level"] = stage_query;
-//  if (type_query != '') dict["category"] = type_query;
-//
-//  console.log(dict);
+  const area_query = req.query.area.replace(/[{|}|']+/g, '');
+  const stage_query = req.query.stage.replace(/[{|}|']+/g, '');
+  const type_query = req.query.type.replace(/[{|}|']+/g, '');
+  const inputText_query = req.query.inputText;
+
+  var dict = {};
+  console.log("-------");
+
+  if (area_query !=  '') dict["location.region"] = area_query;
+  if (type_query != '') dict["category"] = type_query;
+  if (stage_query != '') {
+  // {"menu": { "$elemMatch": {"level": stage_query}}} 형식으로 되어야 함
+  }
+  console.log(dict);
+
+  if (area_query == '' && type_query == '' && stage_query == '' && inputText_query == '') dict["location.region"] = "오류";
+  console.log(dict);
 
   const client = await clientPromise;
   const db = client.db("sample");
 
   const data = await db
     .collection("sam")
-    .find( { "location.region": "강동구" } )
+    .find(dict, {"menu": { "$elemMatch": {"level": "비건"}}})
+    // .find( { "location.region": "강동구" } )
+    // .find({"menu": { "$elemMatch": {"level": "비건"}}})
     .toArray();
 
   console.log(data);
