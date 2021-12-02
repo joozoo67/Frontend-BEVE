9a3522b4c79c4abc7d27421d76f09f3ed8f66c3a
spig0126 <spig0126@gmail.com>
2021-12-03 04:56:36 +0900

    💥 Apply recoil states

--- src/library/useSearchRestaurant.js
+++ src/library/useSearchRestaurant.js
@@ -1,39 +1,41 @@
 import { useEffect, useState } from "react";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+
+import { queryState, restaurantDataState } from "../states";
 
 export default function useSearchRestauarants() {
   const [isLoading, setIsLoading] = useState(null);
   const [isError, setIsError] = useState(null);
-  const [searchQuery, setSearchQuery] = useState({});
 
-  // const [restaurantData, setRestaurantData] = useRecoilState(restaurantDataState);
+  const query = useRecoilValue(queryState);
+  const setRestaurantData = useSetRecoilState(restaurantDataState);
   
   useEffect(() => {
     const fetchData = async () => {
-      setIsLoading(false);
+      setIsLoading(true);
       setIsError(false);
       
-      // try {
-      //   // const response = await axios({
-      //   //   method: "post",
-      //   //   url: "/api/posts",
-      //   //   data: searchQuery
-      //   // });
-      //   // setRestaurantData(response.data);
-      //   // setIsLoading(true);
-      //   // console.log(response.data);
-      //   // console.log(restaurantData);
-      // }
-      // catch (error) {
-      //   setIsError(true);
-      //   console.log(error);
-      // }
-      // finally {
-      //   setIsLoading(false);
-      // }
+      try {
+        const response = await axios({
+          method: "post",
+          url: "/api/posts",
+          data: query
+        });
+        setRestaurantData(response.data);
+        console.log(response.data);
+        console.log(restaurantData);
+      }
+      catch (error) {
+        setIsError(true);
+        console.log(error);
+      }
+      finally {
+        setIsLoading(false);
+      }
     };
     
     fetchData();
-  }, [searchQuery]);
+  }, [query]);
 
-  return { isLoading, isError, searchQuery, setSearchQuery };
+  return { isLoading, isError };
 }
