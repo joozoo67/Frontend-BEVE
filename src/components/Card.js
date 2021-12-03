ac3ce721da1006fa26493b28deabde6484712462
march0320 <march18529@gmail.com>
2021-12-03 21:33:21 +0900

    commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -24,8 +24,8 @@ export default function Card({ value }) {
           onClick={onOpen}
         >
           <DetailedShop onOpen={onOpen} isOpen={isOpen} onClose={onClose} value={value}/>
-          { `../../public/img_res/${value.name}/1.png` ? (
-            <Image src={`../../public/img_res/${value.name}/1.png`} objectFit="fill" maxW="15vw" maxH="13vw" w="15vw" h="13vw" />
+          { `/img_res/${value.name}/1.png` ? (
+            <Image src={`/img_res/${value.name}/1.png`} objectFit="fill" maxW="15vw" maxH="13vw" w="15vw" h="13vw" />
           ):(<Image src={noimage.src} objectFit="fill" maxW="15vw" maxH="13vw" w="15vw" h="13vw" />)
           }
           <Box m={2} textAlign="left">
