7526c48ac773bf749c5c9ed3410ba78a53aaae7e
spig0126 <spig0126@gmail.com>
2021-12-03 14:06:06 +0900

    🚑 Fix 'maximum call stack size exceeded' error

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import DetailedShop from "./DetailedShop";
 
 export default function Card({ value }) {
-  console.log(value);
+  // console.log(value);
   const { isOpen, onOpen, onClose } = useDisclosure()
   return (
     <Flex
@@ -14,29 +14,20 @@ export default function Card({ value }) {
       display="flex"
       flexDirection="column"
       boxShadow="base"
+      h="100%"
+      w="100%"
       color="black"
-      // h="90%"
-      // w="85%"
       onClick={() => {
         onOpen();
         (<DetailedShop value={value} onOpen={onOpen} onClose={onClose} />)
       }}
-      _hover={(
-        <>
-          <Box m={2} textAlign="left">
-            <Text color="green" fontWeight="bolder">{value.name}</Text>
-            <Text fontSize="0.8rem" justifySelf="end">{value.category}</Text>
-            <Text fontSize="0.8rem">{value.location.address}</Text>
-          </Box>
-        </>
-      )}
     >
-      <Image src={`/img_res/${value.name}/1.png`} objectFit="cover" w="100%" h="100%" maxW="100%" maxH="100%" />
-      {/* <Box m={2} textAlign="left">
-        <Text color="green" fontWeight="bolder">{value.name}</Text>
-        <Text fontSize="0.8rem" justifySelf="end">{value.category}</Text>
-        <Text fontSize="0.8rem">{value.location.address}</Text>
-      </Box> */}
+      <Image src={`/img_res/${value.name}/1.png`} objectFit="cover" w="100%" h="80%" maxW="100%" maxH="80%" />
+        <Box m={2} textAlign="left">
+          <Text color="green" fontWeight="bolder">{value.name}</Text>
+          <Text fontSize="0.8rem" justifySelf="end">{value.category}</Text>
+          <Text fontSize="0.8rem">{value.location.address}</Text>
+        </Box> 
       <Spacer />
     </Flex>
   );
