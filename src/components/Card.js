81c4cd708a8201158beb53efa5982fe1e3b37266
march0320 <march18529@gmail.com>
2021-12-03 17:12:39 +0900

    add no-image

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,7 @@
 import { Box, Image, Text, Flex, Spacer, Button,useDisclosure } from "@chakra-ui/react";
 import React, { useState, useEffect } from "react";
 import DetailedShop from "./DetailedShop";
+import noimage from "../../public/img/noimage.jpg"
 
 export default function Card({ value }) {
   // console.log(value);
@@ -19,12 +20,14 @@ export default function Card({ value }) {
           flexDirection="column"
           h="100%"
           w="90%"
-          boxShadow="base"
           color="black"
           onClick={onOpen}
         >
           <DetailedShop onOpen={onOpen} isOpen={isOpen} onClose={onClose} value={value}/>
-          <Image src={`/img_res/${value.name}/1.png`} objectFit="fill" maxW="15vw" maxH="13vw" w="15vw" h="13vw" />
+          { `/img_res/${value.name}/1.png` ? (
+            <Image src={`/img_res/${value.name}/1.png`} objectFit="fill" maxW="15vw" maxH="13vw" w="15vw" h="13vw" />
+          ):(<Image src={noimage.src} objectFit="fill" maxW="15vw" maxH="13vw" w="15vw" h="13vw" />)
+          }
           <Box m={2} textAlign="left">
             <Text color="green" fontWeight="bolder">{value.name}</Text>
             <Text fontSize="0.8rem" justifySelf="end">{value.category}</Text>
@@ -45,12 +48,15 @@ export default function Card({ value }) {
           flexDirection="column"
           h="100%"
           w="90%"
-          boxShadow="base"
           color="black"
           onClick={onOpen}
         >
           <DetailedShop onOpen={onOpen} isOpen={isOpen} onClose={onClose} value={value}/>
-          <Image src={`/img_res/` + tmp_t[0] + `/1.png`} objectFit="fill" maxW="15vw" maxH="13vw" w="15vw" h="13vw" />
+           { `/img_res/` + tmp_t[0] + `/1.png` ? (
+             <Image src={`/img_res/` + tmp_t[0] + `/1.png`} objectFit="fill" maxW="15vw" maxH="13vw" w="15vw" h="13vw" />
+          ):(<Image src={noimage.src} objectFit="fill" maxW="15vw" maxH="13vw" w="15vw" h="13vw" />)
+          }
+
 
           <Box m={2} textAlign="left">
             <Text color="green" fontWeight="bolder">{value.name}</Text>
