8ead063779d7567b6d34d8f617d59e232ca6c723
spig0126 <spig0126@gmail.com>
2021-12-05 00:57:19 +0900

    🚧 Working on image loading error

--- src/components/Card.js
+++ src/components/Card.js
@@ -24,8 +24,8 @@ export default function Card({ value }) {
           onClick={onOpen}
         >
           <DetailedShop onOpen={onOpen} isOpen={isOpen} onClose={onClose} value={value}/>
-          { `../../public/img_res/${value.name}/1.png` ? (
-            <Image src={`../../public/img_res/${value.name}/1.png`} objectFit="fill" maxW="15vw" maxH="13vw" w="15vw" h="13vw" />
+          { `./public/img_res/${value.name}/1.png` ? (
+            <Image src={`./public/img_res/${value.name}/1.png`} objectFit="fill" maxW="15vw" maxH="13vw" w="15vw" h="13vw" />
           ):(<Image src={noimage.src} objectFit="fill" maxW="15vw" maxH="13vw" w="15vw" h="13vw" />)
           }
           <Box m={2} textAlign="left">
@@ -53,8 +53,8 @@ export default function Card({ value }) {
           onClick={onOpen}
         >
           <DetailedShop onOpen={onOpen} isOpen={isOpen} onClose={onClose} value={value}/>
-           { `../../public/img_res/` + tmp_t[0] + `/1.png` ? (
-             <Image src={`../../public/img_res/` + tmp_t[0] + `/1.png`} objectFit="fill" maxW="15vw" maxH="13vw" w="15vw" h="13vw" />
+           { `./public/img_res/` + tmp_t[0] + `/1.png` ? (
+             <Image src={`./public/img_res/` + tmp_t[0] + `/1.png`} objectFit="fill" maxW="15vw" maxH="13vw" w="15vw" h="13vw" />
           ):(<Image src={noimage.src} objectFit="fill" maxW="15vw" maxH="13vw" w="15vw" h="13vw" />)
           }
 
