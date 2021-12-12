62c54a70dd7a1c57ca21cd0c778fd46e86e6fa8a
spig0126 <spig0126@gmail.com>
2021-12-12 16:28:37 +0900

    🚧 Clear FIlter when modal open

--- src/components/filter/Filter.js
+++ src/components/filter/Filter.js
@@ -53,7 +53,10 @@ export default function Filter({ isOpen, onClose, showToast }) {
   return (
     <>
       <Modal
-        isOpen={isOpen}
+        isOpen={() => {
+          clearFilter();
+          isOpen();
+        }}
         onClose={() => {
           onClose();
           clearFilter();
